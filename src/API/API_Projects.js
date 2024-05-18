7a23eb3ba368faad0a13007305c9491deeb04cbd
b4sicpixel <b4sicpixel@gmail.com>
2024-05-18 04:54:02 +0000

    Add news APIS for my portfolio

---
+++ src/API/API_Projects.js
@@ -0,0 +1,7 @@
+export const PROJECTS = [
+    {
+        name: "Project1",
+        description: "Lorem Impsum sabner nsdier nsjasddd poret",
+        img: null,
+    },
+]
---
+++ src/API/API_Social.js
@@ -0,0 +1,4 @@
+
+export const Discord = "dazeddev"
+export const Youtube = "https://www.youtube.com/channel/UCfgw8yJgHxmSTzK6PBSxN5w"
+export const LinkedIn = "https://www.youtube.com/channel/UCfgw8yJgHxmSTzK6PBSxN5w"
