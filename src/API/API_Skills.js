a76860120f8848589248e2350a90f4ae89ded57d
b4sicpixel <b4sicpixel@gmail.com>
2024-05-18 23:41:40 +0000

    New API for my skills in development 🔨

---
+++ src/API/API_Skills.js
@@ -0,0 +1,30 @@
+export const SKILLS = [
+    {
+        id: 1,
+        lenguage: "CSS",
+    },
+    {
+        id: 2,
+        lenguage: "HTML",
+    },
+    {
+        id: 3,
+        lenguage: "JS",
+    },
+    {
+        id: 4,
+        lenguage: "NodeJS",
+    },
+    {
+        id: 5,
+        lenguage: "Tailwind",
+    },
+    {
+        id: 6,
+        lenguage: "Python",
+    },
+    {
+        id: 7,
+        lenguage: "Java",
+    },
+]
