9f5271f552e47ee4931c145b2b6d204e0aaf4079
b4sicpixel <b4sicpixel@gmail.com>
2024-05-19 00:04:10 +0000

    Fixed the color hover on each button

--- src/API/API_Skills.js
+++ src/API/API_Skills.js
@@ -2,29 +2,36 @@ export const SKILLS = [
     {
         id: 1,
         lenguage: "CSS",
+        color: "blue",
     },
     {
         id: 2,
         lenguage: "HTML",
+        color: "orange",
     },
     {
         id: 3,
         lenguage: "JS",
+        color: "yellow",
     },
     {
         id: 4,
         lenguage: "NodeJS",
+        color: "green",
     },
     {
         id: 5,
         lenguage: "Tailwind",
+        color: "sky",
     },
     {
         id: 6,
         lenguage: "Python",
+        color: "purple",
     },
     {
         id: 7,
         lenguage: "Java",
+        color: "red",
     },
 ]
